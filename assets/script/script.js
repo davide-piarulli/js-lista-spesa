e187f33bdf64e9ce548318bb86a52faa83711ea7
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-27 13:58:14 +0000

    init

---
+++ assets/script/script.js
@@ -0,0 +1,27 @@
+/**
+ * 1 creare array con lista spesa fittizia
+ * 
+ */
+
+// 1
+const listaSpesa = [
+  'frutta',
+  'verdura',
+  'carne',
+  'pasta',
+  'formaggio'
+];
+
+
+// elements
+const limit = listaSpesa.length;
+let counter = 0;
+
+
+while (counter < limit) {
+  console.log(counter);
+
+  counter++;
+}
+
+
